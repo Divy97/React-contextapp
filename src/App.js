35d06d823717e40e80f9c9bdad15adc9161f5974
Divy Parekh <divyparekh1810@gmail.com>
2022-06-09 00:23:50 +0530

    Editing TodoForm component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import TodoForm from './components/TodoForm';
 const App = () => {
   return (
     <div className='todo-app'>
-      Hey
+      <TodoForm />
     </div> 
   )
 }

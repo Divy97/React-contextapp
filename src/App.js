4d09db02beecff68051d7f899771c13aebf5c8cd
Divy Parekh <divyparekh1810@gmail.com>
2022-06-09 00:42:01 +0530

    Editing TodoList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 const App = () => {
   return (
     <div className='todo-app'>
-      <TodoForm />
+      <TodoList />
     </div> 
   )
 }
